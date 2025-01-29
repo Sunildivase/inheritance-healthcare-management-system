37d596467c9f7508ac0ebdf45ce911fcf588b12b
Sunil <sunildivase07@gmail.com>
2025-01-29 19:06:53 +0530

    inheritance implement

--- src/com/healthcaremnagement/service/BillingService.java
+++ src/com/healthcaremnagement/service/BillingService.java
@@ -3,7 +3,9 @@ package com.healthcaremnagement.service;
 import com.healthcaremnagement.model.Billing;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class BillingService {
 
@@ -14,10 +16,31 @@ public class BillingService {
     void printBilling(Billing billing){
         System.out.println(billing);
     }
-    public void createBilling(){
-        
+    public Billing createBilling(){
+        System.out.println("please enter bill Id");
+        int billId = Integer.parseInt(sc.nextLine());
+
+        System.out.println("please enter bill");
+        int bill = Integer.parseInt(sc.nextLine());
+
+        System.out.println("please enter total bill");
+        int totalBill = Integer.parseInt(sc.nextLine());
+
+        System.out.println("please enter personId");
+        int personId = Integer.parseInt(sc.nextLine());
+
+        Billing billing = new Billing();
+        billing.setBillId(billId);
+        billing.setBill(bill);
+        billing.setTotalBill(totalBill);
+        billing.setPersonId(personId);
+        billingHashMap.put(String.valueOf(1),billing);
+        return billing;
     }
     public void displayBilling(){
-
+        Set<Map.Entry<String ,Billing>> entrySet = billingHashMap.entrySet();
+        for(Map.Entry<String ,Billing> billingEntry: entrySet){
+            System.out.println("key: "+billingEntry.getKey()+"value: "+billingEntry.getValue());
+        }
     }
 }
